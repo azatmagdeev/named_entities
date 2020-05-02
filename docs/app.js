78d6f93ff1285e673289bf20a5fe1ef733702cca
azatmagdeev <azatmagdeev@ya.ru>
2020-05-02 11:56:46 +0300

    next

--- docs/app.js
+++ docs/app.js
@@ -1,97 +1,59 @@
-// const range = new Range();
-// console.log(range);
-// //
-// const element = document
-//     .getElementsByClassName('col')[0];
-//
-// // range.setStart(
-// //     element, 3
-// // );
-// //
-// // range.setEnd(
-// //     element, 4
-// // );
-//
-// // range.setStartBefore(element.children[1]);
-// // range.setEndAfter(element.children[3]);
-//
-// // range.selectNode(element.children[1]);
-// range.selectNodeContents(element.children[2]);
-// // range2 = range.cloneRange();
-// // console.log(range2);
+// import './books.xml';
 
-//
-// document.getSelection().addRange(range);
-//
-// document.addEventListener('selectionchange',()=> {
-//     // console.log('ok');
-//     const selection = document.getSelection();
-//     // selection.removeAllRanges();
-//     // range.setEndBefore();
-//     // selection.addRange(range);
-//     console.log(selection);
-// // selection.collapseToEnd();
-// });
-// const p = document.getElementById('p');
-// p.addEventListener('dblclick',()=>{
 
 const redBtn = document.getElementById('red');
 const blueBtn = document.getElementById('blue');
 const greenBtn = document.getElementById('green');
-const buttons = [redBtn,blueBtn,greenBtn];
+const buttons = [redBtn, blueBtn, greenBtn];
+const list = document.getElementById('list');
+const parseBtn = document.getElementById('parseBtn');
 
-    document.addEventListener('selectionchange',(e)=>{
-        // console.log(e);
-        const selection = document.getSelection();
-        console.log(selection.toString());
+document.addEventListener('selectionchange', (e) => {
 
-        for (const button of buttons) {
-            button.addEventListener('click',()=>{
-                const range = new Range();
+    const selection = document.getSelection();
+    console.log(selection.toString());
 
-                range.setStart(selection.anchorNode,selection.anchorOffset);
-                range.setEnd(selection.anchorNode,selection.focusOffset);
-                // console.log(range);
-                //
-                // document.getSelection().addRange(range);
+    for (const button of buttons) {
+        button.addEventListener('click', () => {
+            const range = new Range();
 
-                // const str = p.textContent.slice(selection.anchorOffset,selection.focusOffset);
-                const str = range.toString();
-                // console.log({str});
-                if (str === ""){}else{
-                    const li = document.createElement('li');
-                    li.textContent = `"${str}"`;
-                    list.appendChild(li);
-                    const span = document.createElement('span');
-                    span.className = button.className;
-                    range.surroundContents(span);
-                    // selection.removeAllRanges();
-                }
-            });
-        }
-
-
-
-
-
-
-    });
-
-// });
-
-
-
-
-
-textarea = document.getElementById('textarea');
-list = document.getElementById('list');
-textarea.addEventListener('select',(e)=>{
-    console.dir(textarea);
-// textarea.value.length =
-const str = textarea.textContent.slice(textarea.selectionStart,textarea.selectionEnd);
-
-    console.log({str});
-    const li = document.createElement('li');
-    li.textContent = str;
-    list.appendChild(li)
+            range.setStart(selection.anchorNode, selection.anchorOffset);
+            range.setEnd(selection.anchorNode, selection.focusOffset);
+            const str = range.toString();
+            if (str === "") {
+            } else {
+                const li = document.createElement('li');
+                li.textContent = `"${str}"`;
+                list.appendChild(li);
+                const tag = document.createElement(`${button.textContent}`);
+                tag.className = button.className;
+                range.surroundContents(tag);
+            }
+        });
+    }
 });
+
+
+
+
+const parser = new DOMParser();
+
+
+const p = document.getElementById('p');
+const xmlStr = `<?xml version="1.0" encoding="UTF-8"?>
+<text>
+    ${p.innerHTML}
+</text>`;
+parseBtn.addEventListener('click',()=>{
+
+    const xmlDoc = parser.parseFromString(xmlStr, "text/xml");
+    console.log(xmlDoc);
+});
+
+
+
+
+
+
+
+
