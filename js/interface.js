3b6aeb011f1b37b491e0d1acc427b6dd6ab6d889
azatmagdeev <azatmagdeev@ya.ru>
2020-05-05 12:32:17 +0300

    svg basket added

--- js/interface.js
+++ js/interface.js
@@ -72,7 +72,7 @@ export class Interface {
         this.deleteAllBtn.addEventListener('click', () => {
             for (let i = this.listElement.children.length - 1; i >= 0; i--) {
                 const el = this.listElement.children[i];
-                el.querySelector('img').click()
+                el.querySelector('div').click()
             }
         })
 
@@ -123,9 +123,12 @@ export class Interface {
             const li = document.createElement('li');
             li.className = 'mb-1';
             li.innerHTML = element.outerHTML;
-            const removeBtn = document.createElement('img');
-            removeBtn.src = "img/delete.png";
-            removeBtn.style.height = "20px";
+            const removeBtn = document.createElement('div');
+            removeBtn.style.display = 'inline';
+            removeBtn.className = 'ml-2'
+            removeBtn.innerHTML = `<svg  viewBox="0 0 12 16" width="12" height="16" aria-hidden="true">
+                 <path fill-rule="evenodd" d="M11 2H9c0-.55-.45-1-1-1H5c-.55 0-1 .45-1 1H2c-.55 0-1 .45-1 1v1c0 .55.45 1 1 1v9c0 .55.45 1 1 1h7c.55 0 1-.45 1-1V5c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H3V5h1v8h1V5h1v8h1V5h1v8h1V5h1v9zm1-10H2V3h9v1z"></path>
+                 </svg>`
             li.appendChild(removeBtn);
             this.listElement.appendChild(li);
 
